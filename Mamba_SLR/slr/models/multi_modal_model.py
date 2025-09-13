f5e30066509e9504acc925e95c7f8848cbf63d71
Chingiz <tursunbayevchingiz@gmail.com>
2025-09-13 19:35:57 +0900

    multi_modal_model fixed

--- Mamba_SLR/slr/models/multi_modal_model.py
+++ Mamba_SLR/slr/models/multi_modal_model.py
@@ -1,100 +1,62 @@
-# slr/datasets/multi_modal_datasets.py
+# slr/models/multi_modal_model.py
 
-import os
 import torch
-from torch.utils.data import Dataset
-import numpy as np
-from PIL import Image
-import pickle
+import torch.nn as nn
+from .model import Model # Reuse the existing Vision Transformer
 
-class MultiModalPhoenixDataset(Dataset):
-    def __init__(self, image_prefix, qgrid_prefix, kp_path, meta_dir_path, gloss_dict_path, split='train', transforms=None):
-        self.image_prefix = image_prefix
-        self.qgrid_prefix = os.path.join(qgrid_prefix, split)
-        self.transforms = transforms
-        self.split = split
+class MultiModalMamba(nn.Module):
+    def __init__(self, img_cfg, qgrid_cfg, kp_cfg, num_classes, fusion_cfg):
+        super().__init__()
 
-        self.gloss_dict = np.load(gloss_dict_path, allow_pickle=True).item()
-        self.unk_token_id = self.gloss_dict.get('<unk>', 1)
+        self.image_encoder = Model(**img_cfg)
+        self.qgrid_encoder = Model(**qgrid_cfg)
+
+        self.keypoint_encoder = nn.Sequential(
+            nn.Linear(kp_cfg['input_dim'], kp_cfg['model_dim']),
+            nn.ReLU(),
+            nn.LayerNorm(kp_cfg['model_dim'])
+        )
+
+        self.fusion_cross_attention = nn.MultiheadAttention(
+            embed_dim=fusion_cfg['embed_dim'],
+            num_heads=fusion_cfg['num_heads'],
+            dropout=fusion_cfg['dropout'],
+            batch_first=True
+        )
         
-        all_keypoints_data = pickle.load(open(kp_path, 'rb'))
+        fused_dim = self.image_encoder.d_model + fusion_cfg['embed_dim']
+        self.ctc_head = nn.Linear(fused_dim, num_classes)
 
-        info_file = os.path.join(meta_dir_path, f'{split}_info.npy')
-        metadata = np.load(info_file, allow_pickle=True).item()
+    def forward(self, images, qgrids, keypoints):
+        """
+        images: (B, T_img, C, H, W)
+        qgrids: (B, T_qgrid, C, H, W)
+        keypoints: (B, T_img, D)
+        """
+        B, T_img, C_img, H_img, W_img = images.shape
+        images_reshaped = images.view(B * T_img, C_img, H_img, W_img)
+
+        B, T_qgrid, C_qgrid, H_qgrid, W_qgrid = qgrids.shape
+        qgrids_reshaped = qgrids.view(B * T_qgrid, C_qgrid, H_qgrid, W_qgrid)
         
-        self.samples = [v for k, v in metadata.items() if isinstance(v, dict)]
-
-        self.keypoints_data = {}
-        for sample in self.samples:
-            video_id = sample['fileid']
-            for complex_key, data in all_keypoints_data.items():
-                if video_id in complex_key:
-                    self.keypoints_data[video_id] = data
-                    break
-
-    def __len__(self):
-        return len(self.samples)
-
-    def __getitem__(self, index):
-        sample_info = self.samples[index]
-        video_id = sample_info['fileid']
-
-        # --- THIS IS THE FINAL, CORRECTED PART ---
-        # The 'folder' key contains a file pattern like ".../1/*.png".
-        # We must remove the "/*.png" to get the actual directory path.
-        folder_path_from_meta = sample_info['folder'].replace('/*.png', '')
-        image_folder = os.path.join(self.image_prefix, folder_path_from_meta)
-        # --- END OF CORRECTION ---
+        image_features = self.image_encoder(images_reshaped)
+        qgrid_features = self.qgrid_encoder(qgrids_reshaped)
         
-        images = []
-        try:
-            image_files = sorted([f for f in os.listdir(image_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg'))])
+        image_features = image_features.view(B, T_img, -1)
+        qgrid_features = qgrid_features.view(B, T_qgrid, -1)
 
-            for img_file in image_files:
-                img_path = os.path.join(image_folder, img_file)
-                try:
-                    img = Image.open(img_path).convert('RGB')
-                    images.append(img)
-                except Exception:
-                    continue
+        keypoint_features = self.keypoint_encoder(keypoints)
+
+        low_freq_features = image_features + keypoint_features
+
+        qgrid_context, _ = self.fusion_cross_attention(
+            query=low_freq_features,
+            key=qgrid_features,
+            value=qgrid_features
+        )
+
+        final_fused_features = torch.cat([low_freq_features, qgrid_context], dim=-1)
+
+        logits = self.ctc_head(final_fused_features)
         
-        except FileNotFoundError:
-            return None
-        
-        if not images:
-            return None
-
-        if self.transforms:
-            images = self.transforms(images)
-
-        qgrid_path = os.path.join(self.qgrid_prefix, f"{video_id}.npy")
-        qgrid = torch.from_numpy(np.load(qgrid_path)).float()
-
-        if video_id not in self.keypoints_data:
-            return None
-        keypoints_array = self.keypoints_data[video_id]['keypoints']
-        keypoints = torch.from_numpy(keypoints_array).float()
-        
-        keypoints = keypoints.reshape(keypoints.size(0), -1)
-
-        gloss_sequence = sample_info['label'].split()
-        labels = torch.LongTensor([self.gloss_dict.get(g, self.unk_token_id) for g in gloss_sequence])
-
-        return images, qgrid, keypoints, labels
-
-def multi_modal_collate_fn(batch):
-    batch = [b for b in batch if b is not None]
-    if not batch:
-        return (torch.tensor([]), torch.tensor([]), torch.tensor([]), torch.tensor([]), torch.tensor([]), torch.tensor([]))
-
-    images, qgrids, keypoints, labels = zip(*batch)
-
-    padded_images = torch.nn.utils.rnn.pad_sequence(images, batch_first=True, padding_value=0.0)
-    padded_qgrids = torch.nn.utils.rnn.pad_sequence(qgrids, batch_first=True, padding_value=0.0)
-    padded_keypoints = torch.nn.utils.rnn.pad_sequence(keypoints, batch_first=True, padding_value=0.0)
-    padded_labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=0)
-
-    image_lengths = torch.LongTensor([len(img) for img in images])
-    label_lengths = torch.LongTensor([len(lbl) for lbl in labels])
-
-    return padded_images, padded_qgrids, padded_keypoints, padded_labels, image_lengths, label_lengths
+        return logits.permute(1, 0, 2)
